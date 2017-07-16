a0bdc554755de20514848353bbbf7124732ae11d
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 10:04:19 +0700

    Create db.php

---
+++ db.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "mysql.hostinger.in.th";
+$username = "u184683754_esp32";
+$password = "iydgmvot";
+$dbname = "u184683754_esp32";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "UPDATE esp32 SET command='closed' WHERE id=1";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
