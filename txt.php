132c548e71382dd91004fc4a34cbf888d924c7e0
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 10:40:09 +0700

    Update txt.php

--- txt.php
+++ txt.php
@@ -1,6 +1,7 @@
 <?php
 $myfile = fopen("esp32.txt", "w+") or die("Unable to open file!");
-$txt = "closed";
+
+$txt = " . date("Y/m/d") . ";
 fwrite($myfile, $txt);
 newt_delay(10000)
 echo fread($myfile,filesize("esp32.txt"));
