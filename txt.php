95a2c290f488440703fadb6e454f0517af4eb494
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 10:39:15 +0700

    Update txt.php

--- txt.php
+++ txt.php
@@ -2,7 +2,7 @@
 $myfile = fopen("esp32.txt", "w+") or die("Unable to open file!");
 $txt = "closed";
 fwrite($myfile, $txt);
-echo fread($myfile,filesize("esp32.txt"));
 newt_delay(10000)
+echo fread($myfile,filesize("esp32.txt"));
 fclose($myfile);
 ?>
