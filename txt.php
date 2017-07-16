7b6f6ee77bd1c3e65e5295f49123c9749329f9d8
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 10:41:44 +0700

    Update txt.php

--- txt.php
+++ txt.php
@@ -1,7 +1,8 @@
 <?php
+date_default_timezone_set("America/New_York");
 $myfile = fopen("esp32.txt", "w+") or die("Unable to open file!");
 
-$txt = " . date("Y/m/d") . ";
+$txt = " . date("h:i:sa") . ";
 fwrite($myfile, $txt);
 newt_delay(10000)
 echo fread($myfile,filesize("esp32.txt"));
