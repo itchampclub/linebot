3bb7680f74d933e146c2f1ad82bb4337d14e7962
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-16 08:05:41 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,8 +14,8 @@ $_msg = $arrJson['events'][0]['message']['text'];
 
 
 $api_key="raGvU0tka_kLPSFwL7ObSQKwZGR-91G2";
-$url = 'https://api.mlab.com/api/1/databases/itbot/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/itbot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
+$url = 'https://api.mlab.com/api/1/databases/esp32/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/esp32/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$_msg.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
 if (strpos($_msg, '@') !== false) {
