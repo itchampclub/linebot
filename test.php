b57d11052cd77a70485ba7c4f3fefec5f354ca07
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-17 10:11:13 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,14 +1,8 @@
 <?php
 $filename = 'esp32t.txt';
-$_msg = 555
-$_msg2 = 666
-$myfile1 = fopen('esp32t.txt', "w+") or die("Unable to open file!");
-$myfile2 = fopen('esp32t.txt', "x+") or die("Unable to open file!");
 if (file_exists($filename)) {
-  fwrite($myfile1, $_msg);
-  fclose($myfile1);} 
+    echo "The file $filename exists";
 } else {
-  fwrite($myfile2, $_msg2);
-  fclose($myfile2);} 
+    echo "The file $filename does not exist";
 }
 ?>
