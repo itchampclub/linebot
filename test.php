028b24a1b6156b04a0d0f6287956ee6dbb76f534
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-17 10:23:54 +0700

    Update test.php

--- test.php
+++ test.php
@@ -2,6 +2,9 @@
 $filename = 'aa.txt';
 $_msg = 'aabb';
 if (file_exists($filename)) {
+  $myfile = fopen('aa.txt', "w+") or die("Unable to open file!");
+  fwrite($myfile, $_msg);
+  fclose($myfile);     
 echo 'haveeee';
 } else {
   $myfile = fopen('aa.txt', "x+") or die("Unable to open file!");
