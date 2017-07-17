319dfe93b74afeaf7c9125d59f0117e72f1cb17b
Ittirit Thipphinit <itchampclub@gmail.com>
2017-07-17 10:00:37 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,15 @@
 <?php
+$checktxt = file_exists("esp32t.txt");
+$_msg = 555
+$_msg2 = 666
+if ($checktxt = false) {
+  $myfile = fopen('esp32t.txt', "x+") or die("Unable to open file!");
+  fwrite($myfile, $_msg);
+  fclose($myfile);} 
+else {
+  $myfile = fopen('esp32t.txt', "w+") or die("Unable to open file!");
+  fwrite($myfile, $_msg2);
+  fclose($myfile);} 
+}
 echo file_exists("esp32t.txt");
 ?>
